6fe8aa3a3915d716567581cd8990539f33121161
SpaceOfVariations <alecsandr.tiurin@gmail.com>
2020-01-18 16:46:20 +0200

    Lesson11

---
+++ main.js
@@ -0,0 +1,26 @@
+var day = prompt('What day is it today?');
+switch (day) {
+    case "Monday":
+        alert("Working day");
+        break;
+    case "Tuesday":
+        alert("Working day");
+        break;
+    case "Wednesday":
+        alert("Working day");
+        break;
+    case "Thursday":
+        alert("Working day");
+        break;
+    case "Friday":
+        alert("Working day");
+        break;
+    case "Saturday":
+        alert("Weekend");
+        break;
+    case "Sunday":
+        alert("Weekend");
+        break;
+    default:
+        alert("Can not define, please try another value");
+}
